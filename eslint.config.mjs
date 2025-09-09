bf85cbe7bb48fd9da6785510fa004d3c2ce86434
Jan Forst <ensonador@gmail.com>
2025-09-09 15:31:53 +0200

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'eslint/config'
 import maxpat from 'eslint-plugin-maxpat'
 
 export default defineConfig([{
-  files: ['*.maxpat'],
   plugins: { maxpat },
   extends: ['maxpat/recommended'],
   rules: {
