f33f158454d1a26ad648678bac062bbb7b8b6642
Jan Forst <ensonador@gmail.com>
2025-09-19 15:07:48 +0200

    screenshots

--- scripts/screenshots.conf.js
+++ scripts/screenshots.conf.js
@@ -8,11 +8,10 @@ export default {
   devices: [
     [1, 'PX World', 248],
     [1, 'PX Mix View', 676, true],
-    [1, 'PX Shader', 856],
+    [1, 'PX Shader', 856 + 72 * 2],
     [1, 'PX Mix Channel', 276],
     [1, 'PX Mix Dry', 276],
     [1, 'PX FX Transform', 276],
-    [1, 'PX IN Image', 276],
     [2, 'PX Route Audio', 224],
     [2, 'PX GEN Text', 276],
     [2, 'PX IN Syphon', 276],
@@ -37,10 +36,11 @@ export default {
     [4, 'PX FX Colorize', 452],
     [5, 'PX Mix View (expanded)', 1054],
     [5, 'PX Route MIDI (linked)', 224],
-    [5, 'PX Shader (types)', 592],
+    [5, 'PX Shader (types)', 592 + 24],
     [5, 'PX GEN Text (expanded)', 536],
     [5, 'PX FX Alpha', 452],
     [6, 'PX FX Pixelization', 544],
+    [6, 'PX IN Image', 276]
   ].map(([src, name, x1, animated]) => {
     WIDTHS[src] = WIDTHS[src] || 14
     const r = [src, name, WIDTHS[src], x1, animated]
