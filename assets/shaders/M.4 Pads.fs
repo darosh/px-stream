a1cf55dfa6d7dd47e01b44027b42e83a1cbb8c86
Jan Forst <ensonador@gmail.com>
2025-11-11 11:24:00 +0100

    midi shader wip

--- assets/shaders/M.4 Pads.fs
+++ assets/shaders/M.4 Pads.fs
@@ -3,7 +3,9 @@
     "CATEGORIES": ["MIDI Visualizer"],
     "INPUTS": [
         {"NAME": "midiImage", "TYPE": "audio"},
-        {"NAME": "margin", "TYPE": "float", "DEFAULT": 0.1, "MIN": 0, "MAX": 0.5}
+        {"NAME": "margin", "TYPE": "float", "DEFAULT": 0.1, "MIN": 0, "MAX": 1},
+        {"NAME": "edge", "TYPE": "float", "DEFAULT": 0, "MIN": 0, "MAX": 0.5},
+        {"NAME": "minBrigtness", "TYPE": "float", "DEFAULT": 0.25, "MIN": 0, "MAX": 1}
     ],
     "ISFVSN": "2",
     "DESCRIPTION": "4x4 finger drumming pad visualization"
@@ -27,7 +29,8 @@ void main() {
 
     // Grid setup
     float gridSize = 4.0;
-    float cellSize = 1.0 / gridSize;
+    float marginSize = margin / gridSize / 2;
+    float cellSize = (1.0 - marginSize * 2) / gridSize;
 
     vec3 color = vec3(0.1);
 
@@ -36,24 +39,24 @@ void main() {
         for (float col = 0.0; col < gridSize; col += 1.0) {
             // Calculate pad center
             vec2 padCenter = vec2(
-            (col + .5) * cellSize - .5,
-            (row + .5) * cellSize - .5
+            (col + .5) * cellSize - .5 + marginSize,
+            (row + .5) * cellSize - .5 + marginSize
             );
 
             // Distance from current pixel to pad center
             float d = circleSDF(st - padCenter);
 
             // Radius of each pad (with margin)
-            float radius = cellSize * 0.5 * (1.0 - margin);
+            float radius = cellSize * (1.0 - margin);
 
             // Fill the circle
-            float circle = fill(d, radius);
+            float circle = edge > 0. ? fill(d, radius, edge) : fill(d, radius);
 
             // Get activity (dummy - always on)
             float padIndex = col + row * gridSize;
             float activity = getPadActivity(padIndex);
 
-            activity = activity > 0. ? mix(0.5, 1.0, activity) : 0.;
+            activity = activity > 0. ? mix(minBrigtness, 1.0, activity) : 0.;
 
             // Color the pad
             vec3 padColor = vec3(0.2, 0.6, 1.0) * activity;
